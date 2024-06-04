ac4fc0e419a7ea3d911918c89606d17d2c65a34b
ralexale <ralexale@gmail.com>
2024-06-04 08:10:13 -0500

    feat: add repositories

---
+++ src/main/java/com/riwi/filtro/hector/filtro_riwi_hector/domain/repositories/OptionQuestionRepository.java
@@ -0,0 +1,7 @@
+package com.riwi.filtro.hector.filtro_riwi_hector.domain.repositories;
+
+import com.riwi.filtro.hector.filtro_riwi_hector.domain.entities.OptionQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionQuestionRepository extends JpaRepository<OptionQuestion, Long> {
+}
